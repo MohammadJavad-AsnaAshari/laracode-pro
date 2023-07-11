3555531264ad3c7b244550df2977887205989b39
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-07-11 11:54:04 +0330

    add password and email confrim to LaravelPro

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(["verify" => true]);
 Route::get("/auth/google", [\App\Http\Controllers\Auth\GoogleAuthController::class,"redirect"])->name("auth.google");
 Route::get("/auth/google/callback", [\App\Http\Controllers\Auth\GoogleAuthController::class,"callback"]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get("/secret", function (){
+    return "secret";
+})->middleware("password.confirm");
