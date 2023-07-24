0d59220b5362f108380c92d1b74b44d5ca6975d3
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-07-24 12:17:31 +0330

    Fix 'Two Factor Authenticate' and 'Verify Your Email Address' when register or login to website!

--- config/app.php
+++ config/app.php
@@ -169,8 +169,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\BladeServiceProvider::class,
-
     ])->toArray(),
 
     /*
