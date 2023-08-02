1b2e5a6223e28f6e29021931f9465ecf0a771c4f
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-08-02 12:24:16 +0330

    add comments table to LaravelPro

---
+++ app/Models/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "comment", "approved", "parent_id", "commentable_id", "commentable_type"
+    ];
+}
