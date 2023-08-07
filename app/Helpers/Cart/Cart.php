1b6bbc22b7837cc7699b926c7552a543c6401b8b
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-08-07 11:35:32 +0330

    add functions to Card Facade

--- app/Helpers/Cart/Cart.php
+++ app/Helpers/Cart/Cart.php
@@ -2,8 +2,16 @@
 
 namespace App\Helpers\Cart;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Facade;
+use Ramsey\Collection\Collection;
 
+/**
+ * @method static bool has($id);
+ * @method static Collection all();
+ * @method static array get($key);
+ * @method static Cart put(array $value, Model $obj = null);
+ */
 class Cart extends Facade
 {
     protected static function getFacadeAccessor()
