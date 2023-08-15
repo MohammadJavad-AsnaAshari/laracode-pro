dc934bc540f1cda5d585f5ee01606cc6d290156a
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-08-15 19:09:48 +0330

    add file manager

--- config/filesystems.php
+++ config/filesystems.php
@@ -29,6 +29,13 @@ return [
     */
 
     'disks' => [
+        // images folder in public path
+        'images' => [
+            'driver' => 'local',
+            'root' => public_path('images'),
+            'url' => '/images',
+        ],
+
 
         'local' => [
             'driver' => 'local',
@@ -39,7 +46,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -7,6 +7,21 @@
 
     @slot('script')
         <script>
+            document.addEventListener("DOMContentLoaded", function() {
+
+                document.getElementById('button-image').addEventListener('click', (event) => {
+                    event.preventDefault();
+
+                    window.open('/file-manager/fm-button', 'fm', 'width=1400,height=800');
+                });
+            });
+
+            // set file link
+            function fmSetLink($url) {
+                document.getElementById('image_label').value = $url;
+            }
+
+
             $('#categories').select2({
                 'placeholder': 'دسترسی مورد نظر را انتخاب کنید'
             })
@@ -134,7 +149,12 @@
                         </div>
                         <div class="form-group">
                             <label class="col-sm-2 control-label">آپلود تصویر شاخص</label>
-                            <input type="file" name="image" class="form-control" id="inputPassword3">
+                            <div class="input-group">
+                                <input type="text" id="image_label" class="form-control" name="image">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" type="button" id="button-image">انتخاب</button>
+                                </div>
+                            </div>
                         </div>
                         <div class="form-group">
                             <label for="inputEmail3" class="col-sm-2 control-label">دسته بندی ها</label>
