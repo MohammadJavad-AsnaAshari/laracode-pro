1ef261881272cf0708ed7994132725becb8cf10f
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-08-18 18:44:35 +0330

    add Discount Module table to LaravelPro

--- Modules/Discount/Database/Migrations/2023_08_18_143105_create_discounts_table.php
+++ Modules/Discount/Database/Migrations/2023_08_18_143105_create_discounts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,8 +14,26 @@ return new class extends Migration
     {
         Schema::create('discounts', function (Blueprint $table) {
             $table->id();
+            $table->string("code");
+            $table->integer("percent");
+            $table->string("user")->nullable();
 
+            $table->timestamp("expired_at");
             $table->timestamps();
+        });
+
+        Schema::create("discount_product", function (Blueprint $table) {
+            $table->foreignId("discount_id")->references("id")->on("discounts")->onDelete("cascade");
+            $table->foreignId("product_id")->references("id")->on("products")->onDelete("cascade");
+
+            $table->primary(["discount_id", "product_id"]);
+        });
+
+        Schema::create("category_discount", function (Blueprint $table) {
+            $table->foreignId("category_id")->references("id")->on("categories")->onDelete("cascade");
+            $table->foreignId("discount_id")->references("id")->on("discounts")->onDelete("cascade");
+
+            $table->primary(["discount_id", "category_id"]);
         });
     }
 
@@ -27,6 +44,8 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('discount_product');
+        Schema::dropIfExists('category_discount');
         Schema::dropIfExists('discounts');
     }
 };
