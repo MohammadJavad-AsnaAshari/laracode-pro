6f5c6d5fb4a9e21870bfb079877637f99bef5a46
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-08-22 10:46:41 +0330

    add  title and description module json

--- Modules/Main/Resources/views/admin/modules/all.blade.php
+++ Modules/Main/Resources/views/admin/modules/all.blade.php
@@ -15,24 +15,37 @@
                 <div class="card-body">
                     <div class="row">
                         @foreach($modules as $module)
-                            <span>{{$module->getName()}}</span>
-{{--                            <div class="col-sm-2">--}}
-{{--                                <a href="{{ url($image['image']) }}">--}}
-{{--                                    <img src="{{ url($image['image']) }}" class="img-fluid mb-2" alt="{{ url($image['alt']) }}">--}}
-{{--                                </a>--}}
-{{--                                <form action="{{ route('admin.products.gallery.destroy' , ['product' => $product->id , 'gallery' => $image->id]) }}" id="image-{{ $image->id }}" method="post">--}}
-{{--                                    @method('delete')--}}
-{{--                                    @csrf--}}
-{{--                                </form>--}}
-{{--                                <a href="{{ route('admin.products.gallery.edit' , ['product' => $product->id , 'gallery' => $image->id]) }}" class="btn btn-sm btn-primary">ویرایش</a>--}}
-{{--                                <a href="#" class="btn btn-sm btn-danger" onclick="document.getElementById('image-{{ $image->id }}').submit()">حذف</a>--}}
-{{--                            </div>--}}
+                            @php
+                                $moduleData = new \Nwidart\Modules\Json($module->getPath() . "/module.json");
+                            @endphp
+                            <div class="col-sm-2">
+                                <div class="mt-4">
+                                    <h4>{{$moduleData->alias}}</h4>
+                                    <p>{{$moduleData->description}}</p>
+                                </div>
+                                @if(\Module::isEnabled($module))
+                                    <a href="#" class="btn btn-sm btn-danger">غیر فعالسازی</a>
+                                @else
+                                    <a href="" class="btn btn-sm btn-primary">فعالسازی</a>
+                                @endif
+                            </div>
+                            {{--                            <div class="col-sm-2">--}}
+                            {{--                                <a href="{{ url($image['image']) }}">--}}
+                            {{--                                    <img src="{{ url($image['image']) }}" class="img-fluid mb-2" alt="{{ url($image['alt']) }}">--}}
+                            {{--                                </a>--}}
+                            {{--                                <form action="{{ route('admin.products.gallery.destroy' , ['product' => $product->id , 'gallery' => $image->id]) }}" id="image-{{ $image->id }}" method="post">--}}
+                            {{--                                    @method('delete')--}}
+                            {{--                                    @csrf--}}
+                            {{--                                </form>--}}
+                            {{--                                <a href="{{ route('admin.products.gallery.edit' , ['product' => $product->id , 'gallery' => $image->id]) }}" class="btn btn-sm btn-primary">ویرایش</a>--}}
+                            {{--                                <a href="#" class="btn btn-sm btn-danger" onclick="document.getElementById('image-{{ $image->id }}').submit()">حذف</a>--}}
+                            {{--                            </div>--}}
                         @endforeach
                     </div>
                 </div>
                 <!-- /.card-body -->
                 <div class="card-footer">
-{{--                    {{ $images->render() }}--}}
+                    {{--                    {{ $images->render() }}--}}
                 </div>
             </div>
             <!-- /.card -->
